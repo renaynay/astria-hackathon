e0f5d4470e1b6abd24ddc8eef499468359f7d39d
itamar <itamar.reif@gmail.com>
2024-01-22 16:56:10 -0600

    stand up grpc server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	astriaGrpc "buf.build/gen/go/astria/astria/grpc/go/astria/execution/v1alpha2/executionv1alpha2grpc"
+	execution "github.com/renaynay/astria-hackathon/src"
+	"google.golang.org/grpc"
+)
+
 func main() {
 	println("hello, world!")
+	execution_server := execution.NewExecutionServiceServerV1Alpha2()
+	server := grpc.NewServer()
+	astriaGrpc.RegisterExecutionServiceServer(server, execution_server)
+
 }
