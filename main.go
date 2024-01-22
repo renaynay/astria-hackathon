be99e18f4b13f8e3187cb16cb679d1f0d115b22a
itamar <itamar.reif@gmail.com>
2024-01-22 15:48:17 -0600

    imports actually work

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	astriaGrpc "buf.build/gen/go/astria/astria/grpc/go/astria/execution/v1alpha2/executionv1alpha2grpc"
 )
 
-type Server struct {
-	astriaGrpc.UnimplementedExecutionServer
+type ExecutionServiceServerV1Alpha2 struct {
+	astriaGrpc.ExecutionServiceServer
+}
+
+func main() {
+	println("hello, world!")
 }
